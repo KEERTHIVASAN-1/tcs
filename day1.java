13a7a0acd5a62370ae446fadafe8e4c0ae270f7e
KEERTHIVASAN-1 <k32304983@gmail.com>
2026-05-25 20:32:35 +0530

    code 2

--- day1.java
+++ day1.java
@@ -1,24 +1,49 @@
+// PRIME NUMBER
+// import java.util.*;
+// public class day1
+// {
+//     public static void main(String[]args)
+//     {
+//         Scanner input=new Scanner (System.in);
+//         int n=input.nextInt();
+//         if(n<=1)
+//             {
+//                 System.out.print("Not prime");
+//                 return;
+//             }
+//         for(int i=2;i*i<=n;i++)
+//         {
+//             if(n%i==0)
+//             {
+//                 System.out.print("Not Prime");
+//                 return;
+//             }
+            
+//         }
+//         System.out.print("Prime");
+//     }
 import java.util.*;
 public class day1
 {
     public static void main(String[]args)
     {
-        Scanner input=new Scanner (System.in);
+        Scanner input=new Scanner(System.in);
         int n=input.nextInt();
-        if(n<=1)
-            {
-                System.out.print("Not prime");
-                return;
-            }
-        for(int i=2;i*i<=n;i++)
+        int org=n;
+        int rev=0,last;
+        while(n!=0)
         {
-            if(n%i==0)
-            {
-                System.out.print("Not Prime");
-                return;
-            }
-            
+            last=n%10;
+            rev=rev*10+last;
+            n=n/10;
         }
-        System.out.print("Prime");
+        if(rev==org)
+        {
+            System.out.print("palin");
+        }
+        else
+        {
+            System.out.print("not palin");
+        }
     }
 }
