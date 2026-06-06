c599f0b66be1b6f2f292759c28d533567ebc3c42
KEERTHIVASAN-1 <k32304983@gmail.com>
2026-06-06 06:03:27 +0530

    sixth

--- day1.java
+++ day1.java
@@ -122,6 +122,16 @@ public class day1
     public static void main(String[]args)
     {
         Scanner input=new Scanner(System.in);
-        
+        int n=input.nextInt();
+        int a=0;
+        int b=1;
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(a+ " ");
+            c=a+b;
+            a=b;
+            b=c;
+        }
     }
 }
