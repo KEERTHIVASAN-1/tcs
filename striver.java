b43bd08cb98794b36570e38fece782a6f0b511dc
KEERTHIVASAN-1 <k32304983@gmail.com>
2026-06-18 20:40:14 +0530

    prime or not

--- striver.java
+++ striver.java
@@ -52,26 +52,57 @@
     //     }
     // }
     //SUM OF DIGITS OF A NUMBER
-     import java.util.*;
+    //  import java.util.*;
 
-    public class striver {
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            int n = input.nextInt();
-            int last;
-            int sum;
-            while(n>=10)
-            {
-                sum=0;
-                while(n>0)
-                {
-                    last=n%10;
-                    sum=sum+last;
-                    n=n/10;
-                }
-                n=sum;
-            }
-            System.out.print(n);
-        }
-    }
+    // public class striver {
+    //     public static void main(String[] args) {
+    //         Scanner input = new Scanner(System.in);
+    //         int n = input.nextInt();
+    //         int last;
+    //         int sum;
+    //         while(n>=10)
+    //         {
+    //             sum=0;
+    //             while(n>0)
+    //             {
+    //                 last=n%10;
+    //                 sum=sum+last;
+    //                 n=n/10;
+    //             }
+    //             n=sum;
+    //         }
+    //         System.out.print(n);
+    //     }
+    // }
+//PRIME NUMBER OR NOT
+// import java.util.*;
 
+// public class striver
+//  {
+//     public static void main(String[] args) 
+//     {
+//         Scanner input = new Scanner(System.in);
+//         int n = input.nextInt();
+//         boolean flag=true;
+//         if(n<2)
+//         {
+//             System.out.print(false);
+//             return;
+//         }
+//         for(int i=2;i*i<=n;i++)
+//         {
+//             if(n%i==0)
+//             {
+//                 flag=false;
+//                 System.out.print(false);
+//                 break;
+//             }  
+//         }
+//         if(flag)
+//         {
+//             System.out.print(true);
+//         }
+//     }
+// }
+        
+    
