e5cb41cc52679195b92461a2ac0d8a777c38c701
KEERTHIVASAN-1 <k32304983@gmail.com>
2026-06-18 18:13:05 +0530

    PALINDROME IN A RANGE

--- striver.java
+++ striver.java
@@ -25,3 +25,29 @@
 //         }
 //     }
 // }
+//PALINDROME IN A RANGE
+    // import java.util.*;
+
+    // public class striver {
+    //     public static void main(String[] args) {
+    //         Scanner input = new Scanner(System.in);
+
+    //         int n = input.nextInt();
+    //         int m= input.nextInt();
+    //         int temp;
+    //         for(int i=n;i<=m;i++)
+    //         {
+    //             temp=i;
+    //             int rev = 0;
+    //         while (temp > 0) {
+    //             int last = temp % 10;
+    //             rev = rev * 10 + last;
+    //             temp = temp / 10;
+    //         }
+    //         if(rev==i)
+    //         {
+    //             System.out.print(i+ " ");
+    //         }
+    //         }
+    //     }
+    // }
