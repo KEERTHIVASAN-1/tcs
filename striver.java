01e695601f59153313aa6cafd705834672b64d73
KEERTHIVASAN-1 <k32304983@gmail.com>
2026-06-19 16:06:29 +0530

    automorphic

--- striver.java
+++ striver.java
@@ -304,3 +304,33 @@ public class striver
         }
     }
 }
+import java.util.*;
+
+public class striver {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int sq = n * n;
+
+        int count = 0;
+        int temp = n;
+
+        while (temp > 0) {
+            count++;
+            temp = temp / 10;
+        }
+
+        int power = 1;
+
+        for (int i = 1; i <= count; i++) {
+            power = power * 10;
+        }
+
+        if (sq % power == n) {
+            System.out.print("Automorphic Number");
+        } else {
+            System.out.print("Not Automorphic Number");
+        }
+    }
+}
