65ed93c0c0117ecee266f67c797486a714672c33
KEERTHIVASAN-1 <k32304983@gmail.com>
2026-06-24 23:44:16 +0530

    Sum of Numbers in String

--- striver.java
+++ striver.java
@@ -1121,6 +1121,35 @@
 //         System.out.print(another);
 //     }
 // }
+//Sum of Numbers in String
+import java.util.*;
+public class striver
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String s=input.nextLine();
+        int sum=0;
+        int num=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+
+            if(ch>='0' && ch<='9')
+            {
+                num=num*10+(ch-'0');
+            }
+            else
+            {
+                sum=sum+num;
+                num=0;
+            }
+        }
+        sum=sum+num;
+        System.out.print(sum);
+    }
+}
+
 
 
 
